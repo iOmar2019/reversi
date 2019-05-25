72efe90c2a61f21fb77566f29180638d2ea4573a
Omar <omarfilippelli@gmail.com>
2019-05-25 16:16:44 -0700

    Adding Mainjs

---
+++ public/main.js
@@ -0,0 +1,18 @@
+/* functions for general use */
+
+/* This function returns the value associated with 'whichParam' on the URL */
+function getURLParameters(whichParam)
+{
+  var pageURL = window.location.search.substring(1);
+  var pageURLVariables = pageURL.split('&');
+  for(var i = 0; i < pageURLVariables.length; i++){
+    if(parameterName[0] == whichParam){
+      return parameterName[1];
+    }
+  }
+}
+var username = getURLParameters('username');
+if('undefined' == typeof username || !username){
+  username = 'Anonymous_'+Math.random();
+}
+$('#messages')append('<h4>' +username+'</h4>');
